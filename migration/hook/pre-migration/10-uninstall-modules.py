708ad9958d79763fc2a51a40875f570ba2d99b59
Danny de Jong <ddejong@therp.nl>
2024-01-17 21:23:49 +0100

    [IMP] Improved the uninstall script to keep looping until everything is uninstalled.

--- migration/hook/pre-migration/10-uninstall-modules.py
+++ migration/hook/pre-migration/10-uninstall-modules.py
@@ -5,7 +5,7 @@ import sys
 
 
 try:
-    file = open(MIGRATION_PATH + '/etc/uninstall-modules.txt', 'r')
+    file = open(MIGRATION_PATH + "/etc/uninstall-modules.txt", "r")
 except FileNotFoundError:
     exit(0)
 
@@ -15,20 +15,36 @@ for line in file:
     if module_name:
         module_count += 1
 
-logging.info("Uninstalling %i modules..." % module_count,)
-file.seek(0)
-for line in file:
-    module_name = line.strip()
-    if module_name and not module_name.startswith('#'):
-        module = env['ir.module.module'].search([('name', '=', module_name)])
-        if module and module.state in ('installed', 'to upgrade'):
-            logging.info("Uninstalling %s..." % module_name)
-            result = module.button_immediate_uninstall()
-            env.cr.commit()
-            m = env["ir.module.module"].search([('name', '=', module_name)])
-            if m.state in ('installed', 'to upgrade'):
-                raise Exception("Unable to uninstall module %s: %s", module_name, result)
-        else:
-            logging.info("Module %s is not installed!" % module_name)
-logging.info("Done uninstalling modules.")
+logging.info(
+    "Uninstalling %i modules..." % module_count,
+)
 
+module_succeeded = True
+module_failed = False
+while module_succeeded:
+    module_succeeded = False
+    module_failed = False
+    file.seek(0)
+    for line in file:
+        module_name = line.strip()
+        if module_name and not module_name.startswith("#"):
+            module = env["ir.module.module"].search([("name", "=", module_name)])
+            if module and module.state in ("installed", "to upgrade", "to remove"):
+                logging.info("Uninstalling %s..." % module_name)
+                module.button_immediate_uninstall()
+                env.cr.commit()
+                if module.state in ("installed", "to upgrade", "to remove"):
+                    module_failed = True
+                    logging.warn("Failed to remove module %s this time." % module_name)
+                else:
+                    module_succeeded = True
+                    logging.info("Uninstalled %s." % module_name)
+            else:
+                logging.info(
+                    "Module %s is not installed! (%s)" % (module_name, module.state)
+                )
+
+logging.info("Done uninstalling modules.")
+if module_failed:
+    logging.error("Some module(s) still failed to uninstall.")
+    exit(1)
