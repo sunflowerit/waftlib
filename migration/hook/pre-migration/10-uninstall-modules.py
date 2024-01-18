52a2f1f71a6720300345849de47553567ba4129f
Danny de Jong <ddejong@therp.nl>
2024-01-18 15:18:00 +0100

    [IMP] The uninstall script now only sets module states to 'to remove'.

--- migration/hook/pre-migration/10-uninstall-modules.py
+++ migration/hook/pre-migration/10-uninstall-modules.py
@@ -7,44 +7,19 @@ import sys
 try:
     file = open(MIGRATION_PATH + "/etc/uninstall-modules.txt", "r")
 except FileNotFoundError:
+    logging.info("%s/etc/uninstall-modules.txt doesn't exist, no modules will be uninstalled.", MIGRATION_PATH)
     exit(0)
 
-module_count = 0
+module_names = []
 for line in file:
     module_name = line.strip()
     if module_name:
-        module_count += 1
+        module_names.append(module_name)
 
 logging.info(
-    "Uninstalling %i modules..." % module_count,
+    "Uninstalling %i modules..." % len(module_names),
 )
 
-module_succeeded = True
-module_failed = False
-while module_succeeded:
-    module_succeeded = False
-    module_failed = False
-    file.seek(0)
-    for line in file:
-        module_name = line.strip()
-        if module_name and not module_name.startswith("#"):
-            module = env["ir.module.module"].search([("name", "=", module_name)])
-            if module and module.state in ("installed", "to upgrade", "to remove"):
-                logging.info("Uninstalling %s..." % module_name)
-                module.button_immediate_uninstall()
-                env.cr.commit()
-                if module.state in ("installed", "to upgrade", "to remove"):
-                    module_failed = True
-                    logging.warn("Failed to remove module %s this time." % module_name)
-                else:
-                    module_succeeded = True
-                    logging.info("Uninstalled %s." % module_name)
-            else:
-                logging.info(
-                    "Module %s is not installed! (%s)" % (module_name, module.state)
-                )
-
-logging.info("Done uninstalling modules.")
-if module_failed:
-    logging.error("Some module(s) still failed to uninstall.")
-    exit(1)
+for module in env["ir.module.module"].search([("name", "in", module_names)]):
+    module.state = 'to remove'
+env.cr.commit()
