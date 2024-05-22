392bdc08d7aec136811880f429c1a7e62468c123
Danny de Jong <ddejong@therp.nl>
2024-05-22 12:05:45 +0200

    [FIX] [no task] Fix python2.7 support for the opening of files in the install - and uninstall scripts.

--- migration/hook/post-migration/50-install-modules.py
+++ migration/hook/post-migration/50-install-modules.py
@@ -2,12 +2,23 @@
 # etc/post-migration-install-modules.txt
 import os
 import sys
+import traceback
 
 
 try:
     file = open(MIGRATION_PATH + '/etc/install-modules.txt', 'r')
-except FileNotFoundError:
-    exit(0)
+except Exception as e:
+    if type(e) == "FileNotFoundError":
+        logging.warning("%s/etc/uninstall-modules.txt doesn't exist, no modules will be uninstalled.", MIGRATION_PATH)
+        exit(0)
+    if isinstance(e, IOError):
+        logging.warning("%s/etc/uninstall-modules.txt can't be opened, no modules will be uninstalled", MIGRATION_PATH)
+        traceback.print_exc()
+        exit(0)
+    # else:
+    logging.error("Unable to open %s/etc/uninstall-modules.txt:", MIGRATION_PATH)
+    traceback.print_exc()
+    exit(1)
 
 module_count = 0
 for line in file:
--- migration/hook/pre-migration/10-uninstall-modules.py
+++ migration/hook/pre-migration/10-uninstall-modules.py
@@ -2,13 +2,23 @@
 # etc/uninstall-modules.txt
 import os
 import sys
+import traceback
 
 
 try:
     file = open(MIGRATION_PATH + "/etc/uninstall-modules.txt", "r")
-except FileNotFoundError:
-    logging.info("%s/etc/uninstall-modules.txt doesn't exist, no modules will be uninstalled.", MIGRATION_PATH)
-    exit(0)
+except Exception as e:
+    if type(e) == "FileNotFoundError":
+    	logging.warning("%s/etc/uninstall-modules.txt doesn't exist, no modules will be uninstalled.", MIGRATION_PATH)
+        exit(0)
+    if isinstance(e, IOError):
+        logging.warning("%s/etc/uninstall-modules.txt can't be opened, no modules will be uninstalled", MIGRATION_PATH)
+        traceback.print_exc()
+    	exit(0)
+    # else:
+    logging.error("Unable to open %s/etc/uninstall-modules.txt:", MIGRATION_PATH)
+    traceback.print_exc()
+    exit(1)
 
 module_names = []
 for line in file:
