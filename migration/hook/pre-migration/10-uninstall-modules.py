fe54fb40289c34c0d6d3614d4bcedc265215e95b
Danny de Jong <ddejong@therp.nl>
2024-05-23 16:12:35 +0200

    [FIX] tab/space issue in scripts

--- migration/hook/pre-migration/10-uninstall-modules.py
+++ migration/hook/pre-migration/10-uninstall-modules.py
@@ -9,12 +9,12 @@ try:
     file = open(MIGRATION_PATH + "/etc/uninstall-modules.txt", "r")
 except Exception as e:
     if type(e) == "FileNotFoundError":
-    	logging.warning("%s/etc/uninstall-modules.txt doesn't exist, no modules will be uninstalled.", MIGRATION_PATH)
+        logging.warning("%s/etc/uninstall-modules.txt doesn't exist, no modules will be uninstalled.", MIGRATION_PATH)
         exit(0)
     if isinstance(e, IOError):
         logging.warning("%s/etc/uninstall-modules.txt can't be opened, no modules will be uninstalled", MIGRATION_PATH)
         traceback.print_exc()
-    	exit(0)
+        exit(0)
     # else:
     logging.error("Unable to open %s/etc/uninstall-modules.txt:", MIGRATION_PATH)
     traceback.print_exc()
