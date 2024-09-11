43bae51d1581a01ddb2f1051f409022ae8754c01
Danny de Jong <ddejong@therp.nl>
2024-09-11 11:51:41 +0200

    [FIX] #10718 Don't delete any groups, they are not set by default

--- migration/build-14.0/hook/pre-migration/50-mail_channel_default_fix.sql
+++ migration/build-14.0/hook/pre-migration/50-mail_channel_default_fix.sql
@@ -1,5 +1,5 @@
 -- X-Modules: mail
-UPDATE mail_channel SET group_public_id = NULL WHERE channel_type <> 'channel';
-DELETE FROM mail_channel_res_groups_rel WHERE mail_channel_id IN (
-	SELECT id FROM mail_channel WHERE channel_type <> 'channel'
+UPDATE mail_channel SET group_public_id = NULL
+WHERE channel_type <> 'channel' AND group_public_id = (
+	SELECT res_id FROM ir_model_data WHERE module = 'base' AND name = 'group_user'
 );
