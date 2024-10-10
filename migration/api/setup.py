5baee753a5b90a345463eeab1a9d52c577adb0fb
Danny de Jong <ddejong@therp.nl>
2024-10-10 18:20:12 +0200

    [FIX] Some fixes to the cleanup functions in the migration api

--- migration/api/setup.py
+++ migration/api/setup.py
@@ -3,19 +3,19 @@ import sys
 
 
 if sys.version_info.major == 2:
-    required_packages = ['psycopg2']
+    required_packages = ["psycopg2"]
 else:
-    required_packages = ['psycopg']
+    required_packages = ["psycopg"]
 
 
 setup(
-    name='migrationapi',
-    version='1.0',
-    description='Python API for migration scripts',
-    author='Therp B.V.',
-    packages=['migrationapi'],
+    name="migrationapi",
+    version="1.0",
+    description="Python API for migration scripts",
+    author="Therp B.V.",
+    packages=["migrationapi"],
     install_requires=required_packages,
     package_dir={
-        'migrationapi': '.',
+        "migrationapi": ".",
     },
 )
