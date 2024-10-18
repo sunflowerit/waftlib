c669cc29eb5f1061b085674c2a2d9b033bbd92af
Danny de Jong <ddejong@therp.nl>
2024-10-18 12:03:18 +0200

    [IMP] Blacked the migration scripts

--- migration/hook/common/clean-models.py
+++ migration/hook/common/clean-models.py
@@ -5,8 +5,8 @@ missing_models = []
 env.cr.execute("SELECT id, model FROM ir_model")
 results = env.cr.dictfetchall()
 for result in results:
-    model_id = result['id']
-    model_name = result['model']
+    model_id = result["id"]
+    model_name = result["model"]
 
     if not model_name in env:
         logging.info("Purging model %s...", model_name)
