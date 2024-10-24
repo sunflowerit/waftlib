20637be51fdce5de012b4fec1daecbcb1d724290
Danny de Jong <ddejong@therp.nl>
2024-10-24 14:43:55 +0200

    [FIX] Fix action cleanup script

--- migration/hook/pre-migration/50-clean-actions.sql
+++ migration/hook/pre-migration/50-clean-actions.sql
@@ -7,7 +7,7 @@ DELETE FROM ir_actions WHERE "type" = 'ir.actions.server' AND id NOT IN (
 DELETE FROM ir_actions WHERE "type" = 'ir.actions.act_url' AND id NOT IN (
     SELECT id FROM ir_act_url
 );
-DELETE FROM ir_actions WHERE "type" = 'ir.actions.window' AND id NOT IN (
+DELETE FROM ir_actions WHERE "type" = 'ir.actions.act_window' AND id NOT IN (
     SELECT id FROM ir_act_window
 );
 DELETE FROM ir_actions WHERE "type" = 'ir.actions.client' AND id NOT IN (
