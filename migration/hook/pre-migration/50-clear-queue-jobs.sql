2da199761b2dd000a5ea52c9dfc3a4f179339433
Danny de Jong <ddejong@therp.nl>
2024-10-30 07:47:53 +0100

    [FIX] Delete queue jobs, don't truncate them. Truncate will always trigger foreign key constraints even if not necessary.

--- migration/hook/pre-migration/50-clear-queue-jobs.sql
+++ migration/hook/pre-migration/50-clear-queue-jobs.sql
@@ -1,2 +1,4 @@
 -- X-Modules: queue_job
-TRUNCATE TABLE queue_job, queue_job_queue_jobs_to_done_rel, queue_job_queue_requeue_job_rel;
+DELETE FROM queue_job_queue_jobs_to_done_rel;
+DELETE FROM queue_job_queue_requeue_job_rel;
+DELETE FROM queue_job;
