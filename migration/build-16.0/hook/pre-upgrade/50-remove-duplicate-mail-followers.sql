2d7b4f9e433db625f123efa8eeb38ef79a383527
Danny de Jong <ddejong@therp.nl>
2025-03-20 13:50:52 +0100

    [FIX] Remove duplicate mail followers

--- migration/build-16.0/hook/pre-upgrade/50-remove-duplicate-mail-followers.sql
+++ migration/build-16.0/hook/pre-upgrade/50-remove-duplicate-mail-followers.sql
@@ -1,5 +1,11 @@
 -- X-Modules: mail
 
+DELETE FROM mail_followers AS a USING mail_followers AS b
+WHERE a.id < b.id
+AND a.res_model = b.res_model
+AND a.res_id = b.res_id
+AND a.partner_id = b.partner_id;
+
 DELETE FROM mail_notification AS a USING mail_notification AS b
 WHERE a.id < b.id
 AND a.mail_message_id = b.mail_message_id
