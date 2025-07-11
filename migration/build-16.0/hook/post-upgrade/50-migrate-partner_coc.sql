ca8630ead7cdc8803dcc7553149639a9717febf1
Danny de Jong <ddejong@therp.nl>
2025-07-11 15:59:11 +0200

    [ADD] #12473 Add the migration scrip I created for the partner_coc module to our migration framework

---
+++ migration/build-16.0/hook/post-upgrade/50-migrate-partner_coc.sql
@@ -0,0 +1,13 @@
+-- X-Modules: partner_coc
+
+WITH category AS (
+	SELECT res_id AS id FROM ir_model_data
+	WHERE module = 'partner_coc' AND name = 'id_category_coc'
+	LIMIT 1
+)
+UPDATE res_partner rp SET company_registry = r.name
+FROM (
+	SELECT partner_id, name FROM res_partner_id_number
+	WHERE category_id = (SELECT id FROM category)
+) AS r
+WHERE rp.id = r.partner_id;
