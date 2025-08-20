2e0ab9a8f4a6f391ba0d6b23333423ebad54ee6a
Danny de Jong <ddejong@therp.nl>
2025-08-20 15:40:57 +0200

    [FIX] For migrating partner_coc, apply the data after the upgrade

--- migration/build-16.0/hook/pre-upgrade/50-migrate-partner_coc.sql
+++ migration/build-16.0/hook/pre-upgrade/50-migrate-partner_coc.sql
@@ -1,13 +1,13 @@
 -- X-Modules: partner_coc
 
-ALTER TABLE res_partner ADD COLUMN company_registry VARCHAR;
+ALTER TABLE res_partner ADD COLUMN company_registry_temp VARCHAR;
 
 WITH category AS (
 	SELECT res_id AS id FROM ir_model_data
 	WHERE module = 'partner_coc' AND name = 'id_category_coc'
 	LIMIT 1
 )
-UPDATE res_partner rp SET company_registry = r.name
+UPDATE res_partner rp SET company_registry_temp = r.name
 FROM (
 	SELECT partner_id, name FROM res_partner_id_number
 	WHERE category_id = (SELECT id FROM category)
